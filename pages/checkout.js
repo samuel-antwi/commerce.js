787801eda090cb68b7403fa43014d89b09adccde
Samuel Antwi <samuelantwidev@gmail.com>
2021-05-13 23:11:35 +0100

    Checkout

--- pages/checkout.js
+++ pages/checkout.js
@@ -25,31 +25,37 @@ const Checkout = () => {
   }, [cart])
 
   return (
-    <main className='min-h-screen max-w-[85rem] mx-auto pt-10 pb-20 px-5 2xl:px-0'>
-      <div className='flex items-center mb-6 space-x-2 text-sm '>
-        <Link href='/'>
-          <a className='text-gray-700 hover:underline'>Basket</a>
-        </Link>
-        <IoMdArrowDropright />
-        <h1 className='hover:underline'>checkout</h1>
-      </div>
-      {checkoutToken ? (
-        <div className='grid-cols-5 gap-10 lg:grid'>
-          <div className='col-span-2 lg:hidden'>
-            <Order checkoutToken={checkoutToken} />
-          </div>
-          <div className='col-span-3'>
-            <CheckoutForm checkoutToken={checkoutToken} />
-          </div>
-          <div className='hidden col-span-2 lg:block'>
-            <Order checkoutToken={checkoutToken} />
-          </div>
-        </div>
-      ) : (
-        <LoadingScreen />
-      )}
-    </main>
+    <div>
+      <h1>Checkout</h1>
+    </div>
   )
+
+  // return (
+  //   <main className='min-h-screen max-w-[85rem] mx-auto pt-10 pb-20 px-5 2xl:px-0'>
+  //     <div className='flex items-center mb-6 space-x-2 text-sm '>
+  //       <Link href='/'>
+  //         <a className='text-gray-700 hover:underline'>Basket</a>
+  //       </Link>
+  //       <IoMdArrowDropright />
+  //       <h1 className='hover:underline'>checkout</h1>
+  //     </div>
+  //     {checkoutToken ? (
+  //       <div className='grid-cols-5 gap-10 lg:grid'>
+  //         <div className='col-span-2 lg:hidden'>
+  //           <Order checkoutToken={checkoutToken} />
+  //         </div>
+  //         <div className='col-span-3'>
+  //           <CheckoutForm checkoutToken={checkoutToken} />
+  //         </div>
+  //         <div className='hidden col-span-2 lg:block'>
+  //           <Order checkoutToken={checkoutToken} />
+  //         </div>
+  //       </div>
+  //     ) : (
+  //       <LoadingScreen />
+  //     )}
+  //   </main>
+  // )
 }
 
 export default Checkout
